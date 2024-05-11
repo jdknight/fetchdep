bec1fba1b6c94f6a0101b2cc9bbbb11da54d5d95
James Knight <james.d.knight@live.com>
2024-05-11 18:10:37 -0400

    tests: correct cvs prepare on linux
    When trying to build a module on itself in Linux (newer CVS), the tool
    may complain with the following error:
    
        Cannot check out files into the repository itself
    
    To handle this use case, build the CVS module in an interim location.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- tests/extract-tests/test_cvs.py
+++ tests/extract-tests/test_cvs.py
@@ -5,6 +5,7 @@
 from fetchdep.util.io import execute
 from fetchdep.util.io import makedirs
 from tests import FetchdepExtractTestCase
+from tests import generate_temp_dir
 from tests import interim_working_dir
 from tests import prepare_testenv
 import os
@@ -17,10 +18,16 @@ TEST_MODULE = 'module-test'
 class TestToolCvs(FetchdepExtractTestCase):
     def test_cvs(self):
         # prepare a cvs repository
-        self._cvs('init')
-        self._cvs('checkout', '.')
-        makedirs(os.path.join(self.repo_dir, TEST_MODULE))
-        self._cvs('add', TEST_MODULE)
+        with interim_working_dir(self.repo_dir):
+            self._cvs('init')
+
+        # build cvs module outside path, since linux-cvs will not be happy
+        #  (Cannot check out files into the repository itself)
+        with generate_temp_dir() as tmpdir, interim_working_dir(tmpdir):
+            self._cvs('checkout', '.')
+            makedirs(TEST_MODULE)
+            self._cvs('add', TEST_MODULE)
+            self._cvs('commit', '-m', 'test')
 
         # prepare the engine
         with prepare_testenv() as engine:
@@ -48,11 +55,10 @@ class TestToolCvs(FetchdepExtractTestCase):
         # configure CVSROOT to the repository
         new_args = ('-d', self.repo_dir) + args
 
-        with interim_working_dir(self.repo_dir):
-            out = []
-            if execute(['cvs'] + list(new_args), capture=out) != 0:
-                print(['cvs'] + list(new_args))
-                print('\n'.join(out))
-                msg = 'failed to issue cvs command'
-                raise AssertionError(msg)
-            return '\n'.join(out)
+        out = []
+        if execute(['cvs'] + list(new_args), capture=out) != 0:
+            print(['cvs'] + list(new_args))
+            print('\n'.join(out))
+            msg = 'failed to issue cvs command'
+            raise AssertionError(msg)
+        return '\n'.join(out)
